4c07e917783fba5712694240df02c1facef2e291
manaki <mana23happy@hotmail.co.jp>
2020-04-08 18:47:15 +0900

    :+1:footerの追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,14 +4,17 @@
     <main class="l-content">
       <nuxt />
     </main>
+    <l-footer />
   </div>
 </template>
 
 <script>
 import LHeader from "@/components/layout/Header"
+import LFooter from "@/components/layout/Footer"
 export default {
   components: {
     LHeader,
+    LFooter,
   },
 }
 </script>
