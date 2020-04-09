0c20cb6d8c6c480a419a70411835f887007095b0
manaki <mana23happy@hotmail.co.jp>
2020-04-09 12:59:27 +0900

    :+1:Heroセクションの作成

---
+++ pages/member/-cHero.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="c-hero">
+    <div class="c-hero__bg">
+      <div class="c-container">
+        <h2 class="c-hero__title">
+          メンバー紹介<br />
+          <span class="c-hero__title--small">
+            株式会社chatboxのメンバー紹介ページです。
+          </span>
+        </h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.c-hero {
+  &__bg {
+    background-image: url("~assets/image/hero/img_memberTop@2x.png");
+  }
+}
+</style>
---
+++ pages/member/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <c-hero />
+  </section>
+</template>
+
+<script>
+import cHero from "@/pages/member/-cHero"
+export default {
+  components: {
+    cHero,
+  },
+}
+</script>
