5d65d793f41ed2fb0dcc97bea31acae5c2cdd415
manaki <mana23happy@hotmail.co.jp>
2020-04-10 17:03:59 +0900

    :+1:introセクションの追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
   <section class="p-members">
     <c-hero />
+    <c-intro />
   </section>
 </template>
 
 <script>
 import CHero from "@/pages/-cHero.vue"
-
+import CIntro from "@/pages/-cIntro.vue"
 export default {
   components: {
     CHero,
+    CIntro,
   },
 }
 </script>
