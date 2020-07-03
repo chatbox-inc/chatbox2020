6ebd3ce2f8619b18166af1432176e63213689c85
manak1 <mana23happy@hotmail.co.jp>
2020-07-03 18:50:45 +0900

    style: code large button component

---
+++ components/common/CLargeButton.vue
@@ -0,0 +1,57 @@
+<template>
+  <a
+    href="#"
+    class="btn-large-sp lg:btn-large-pc lg:hover:btn-large-pressed inline-block"
+  >
+    <div class="flex items-center bg-white">
+      <img
+        :src="image"
+        class="btn-large-spImg lg:btn-large-pcImg object-cover"
+        alt="イメージ画像"
+      />
+      <p class="text-primary mx-auto text-sm lg:text-2xl font-bold">
+        {{ text }}
+      </p>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.c-largeButton {
+  width: 800px;
+  border: 1px solid #c3504f;
+
+  &__img {
+    width: 30%;
+    height: 140px;
+  }
+
+  &__shadow {
+    box-shadow: 7px 7px 0 0 #c3504f;
+    transition: all 0.3s ease-in;
+
+    &:hover {
+      transform: translate(4px, 4px);
+      -webkit-box-shadow: 3px 3px 0 0 #c3504f;
+      box-shadow: 3px 3px 0 0 #c3504f;
+      -webkit-transition: all 0.3s;
+      transition: all 0.3s ease-in-out;
+    }
+  }
+}
+</style>
--- pages/-CCreation.vue
+++ pages/-CCreation.vue
@@ -1,36 +1,41 @@
 <template>
   <section class="pb-10">
-    <div class="container mx-auto">
-      <h2 class="section-title">
-        <span class="text-primary text-7xl">C</span>reation
-      </h2>
+    <div class="c-creation__head">
+      <div class="container mx-auto">
+        <h2 class="section-title">
+          <span class="text-primary text-7xl">C</span>reation
+        </h2>
+      </div>
+    </div>
+    <div class="container mx-auto pt-3">
       <div class="flex space-x-12">
         <div class="w-1/2">
-          <h3 class="text-primary text-2xl">
+          <h3 class="text-primary text-2xl tracking-widest">
             高い技術力を生かしたWeb制作
           </h3>
-          <p class="leading-9 mt-4">
+          <p class="leading-9 mt-4 tracking-wider">
             chatbox の強みは高い技術力。
             様々なWeb制作、システム構築の制作・導入経験はもちろんのこと、
             日々、新しい技術を取り入れるために、定期的な技術交流イベント開催やセミナー登壇などを行っております。
           </p>
-          <p class="leading-9">
+          <p class="leading-9 tracking-wider">
             複数の現場で技術顧問として通用する chatbox
             の技術力だからこそ、お客様それぞれの課題に、適切な提案とサポートを提供できます。
           </p>
-          <p class="leading-9">
+          <p class="leading-9 tracking-wider">
             未来のビジネスを支える「Webのこれから」をお客様とともに見つけ出せる力が、chatboxの技術力です。
           </p>
-          <figure>
+          <figure class="mt-12">
             <img
               src="@/assets/image/creation/creation__photo01.jpg"
               alt="制作イメージ"
+              class="c-box__img w-full object-cover"
             />
           </figure>
         </div>
         <div class="w-1/2">
           <img
-            class="c-intro__img"
+            class="c-creation__img w-full object-cover"
             src="@/assets/image/creation/creation__photo02.jpg"
             alt=""
           />
@@ -44,48 +49,47 @@
           </ul>
         </div>
       </div>
+      <div class="text-center mt-12 c-creation__bg">
+        <c-large-button
+          :image="'/image/creation/creation_btn.jpg'"
+          :text="'Web制作について'"
+        />
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-export default {}
+import CLargeButton from '@/components/common/CLargeButton'
+export default {
+  components: {
+    CLargeButton,
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-.c-intro {
-  &__bg {
-    padding-top: 250px;
-    background-image: url(~assets/image/intro/intro__bg.jpg);
-    background-size: contain;
-    background-repeat: no-repeat;
-  }
-
-  &__box {
-    padding-right: 10%;
-    background: -webkit-gradient(
-      linear,
-      right top,
-      left top,
-      from(hsla(0, 0%, 99%, 0)),
-      color-stop(10%, hsla(0, 0%, 99%, 0.5)),
-      color-stop(20%, hsla(0, 0%, 100%, 0.8)),
-      color-stop(25%, hsla(0, 0%, 100%, 0.85)),
-      color-stop(61%, hsla(0, 0%, 100%, 0.9)),
-      color-stop(80%, #fff)
-    );
+.c-creation {
+  &__head {
+    background: linear-gradient(#fff 50%, #faf7f7 0);
   }
 
   &__img {
-    width: auto;
     height: 500px;
   }
 }
 
 .c-list {
   &__item {
+    font-size: 17px;
     writing-mode: vertical-lr;
     text-orientation: upright;
+  }
+}
+
+.c-box {
+  &__img {
+    height: 320px;
   }
 }
 </style>
