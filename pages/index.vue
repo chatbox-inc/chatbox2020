6a07d8f367c8abfefb62a810f8aae45626559b05
manak1 <mana23happy@hotmail.co.jp>
2020-07-03 16:18:12 +0900

    style: load components

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
-  <div style="padding-bottom: 200px;">
+  <div>
     <c-hero />
     <c-intro />
     <c-creation />
+    <c-talking />
+    <c-enjoy />
+    <c-member />
+    <c-recruit />
+    <c-news />
+    <c-about />
+    <c-contact />
   </div>
 </template>
 
@@ -10,11 +17,26 @@
 import CHero from '@/pages/-cHero'
 import CIntro from '@/pages/-CIntro'
 import CCreation from '@/pages/-CCreation'
+import CTalking from '@/pages/-CTalking'
+import CEnjoy from '@/pages/-CEnjoy'
+import CMember from '@/pages/-CMember'
+import CRecruit from '@/pages/-CRectruit'
+import CNews from '@/pages/-CNews'
+import CAbout from '@/pages/-CAbout'
+import CContact from '@/pages/-CContact'
+
 export default {
   components: {
     CHero,
     CIntro,
     CCreation,
+    CTalking,
+    CEnjoy,
+    CMember,
+    CRecruit,
+    CNews,
+    CAbout,
+    CContact,
   },
 }
 </script>
