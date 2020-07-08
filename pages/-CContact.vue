e9dee3b7a759745123579f579dc574886d8f50c1
kawamuramika <pual7727@gmail.com>
2020-07-08 16:14:44 +0900

    sp時のコンテナにpaddingを追加

--- pages/-CContact.vue
+++ pages/-CContact.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="pt-16 pb-10">
-    <div class="container mx-auto text-center">
+    <div class="container mx-auto px-8 text-center">
       <ui-title :title="'Contact'" />
-      <p class="mx-auto text-black text-left mb-10 w-4/5 lg:w-3/5">
+      <p class="mx-auto text-black text-left mb-10 w-full lg:w-3/5">
         Web制作のご相談、技術顧問に関するご相談、イベント・スクールに関するお問い合わせなど、
         chatboxに関する相談・ご質問はお気軽に下記「お問い合わせフォーム」からお願いいたします。
       </p>
