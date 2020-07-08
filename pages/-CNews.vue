d6b3faffc97a4a189acc750c03c18bd1e2230b14
NaNagaiShiho <shiho@nagaishihonoMacBook-Pro.local>
2020-07-08 10:46:10 +0900

    リント修正

--- pages/-CNews.vue
+++ pages/-CNews.vue
@@ -1,6 +1,23 @@
 <template>
-  <section>
-    <div class="container mx-auto"></div>
+  <section class="pt-16 pb-10">
+    <div class="container mx-auto">
+      <h2 class="section-title">
+        <span class="text-primary text-7xl">N</span>wes
+      </h2>
+      <div class="flex flex-row bg-gray-1024">
+        <p class="inline-block leading-9 mt-4 pr-5 text-xl">
+          2020/55/55
+        </p>
+        <a class="bg-primary">
+          <p class="h-8 w-32 text-xs text-center px-4 py-2 m-2g">
+            プレリリース
+          </p>
+        </a>
+        <space class="inline-grid grid-cols-1 col-gap-4 pl-5 leading-9 text-xl">
+          お知らせ/お知らせ/お知らせ/お知らせ/お知らせ/お知らせ/お知らせ/お知らせ/お知らせ/お知らせ/お知らせ/お知らせ/お知らせ/お知らせ/お知らせ/お知らせ/お知
+        </space>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -10,5 +27,14 @@ export default {}
 
 <style lang="scss" scoped>
 .c-news {
+  &__tag {
+    font-size: 12px;
+    background-color: rgb(195, 80, 79);
+    position: absolute;
+    width: 114px;
+    height: 26px;
+    text-align: center;
+    line-height: 26px;
+  }
 }
 </style>
