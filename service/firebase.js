bb9e45c93015fd91d666d2efe73fa0ccf905658a
manak1 <mana23happy@hotmail.co.jp>
2020-07-09 12:23:54 +0900

    chore: install firebase

---
+++ service/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const fierbaseConfig = {
+  apiKey: 'AIzaSyCIfQPTBbAWN0V64gMP-3fivC5JUPPahWM',
+  authDomain: 'chatbox2020-26423.firebaseapp.com',
+  databaseURL: 'https://chatbox2020-26423.firebaseio.com',
+  projectId: 'chatbox2020-26423',
+  storageBucket: 'chatbox2020-26423.appspot.com',
+  messagingSenderId: '242233420493',
+  appId: '1:242233420493:web:3c9af8562c50efc7211784',
+  measurementId: 'G-HK4N2HNG5Q',
+}
+
+firebase.initializeApp(fierbaseConfig)
