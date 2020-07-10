30f223777bfd9683fbff3e40fbb6785c7eda65ff
ririko <ririmohu25@gmail.com>
2020-07-10 17:19:07 +0900

    aboutUsの修正

--- pages/-CAbout.vue
+++ pages/-CAbout.vue
@@ -1,8 +1,10 @@
 <template>
-  <section class="pb-20">
+  <section class="lg:py-20 pt-14 pb-20">
     <div class="container mx-auto text-center px-8 sm:px-0">
       <UiTitle :title="'About Us'" />
-      <p class="mx-auto text-black mb-10 text-left w-full lg:w-3/5">
+      <p
+        class="mx-auto text-black mb-10 leading-loose text-left w-full lg:w-3/5"
+      >
         (仮)chatboxについてこの文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
         この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。
       </p>
