7cb270a1f9d9a890ea4a25e29d753c5c0ca2656a
kawamuramika <pual7727@gmail.com>
2020-07-15 16:36:31 +0900

    titleセクション追加

--- pages/company/-CTitle.vue
+++ pages/company/-CTitle.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="c-title__bg">
-    aaa
+  <div class="c-title__bg h-325px bg-cover bg-bottom">
+    <div class="container mx-auto h-325px px-5 sm:px-0">
+      <h2 class="text-white text-2xl lg:text-35px pt-16 pb-8">
+        会社概要
+      </h2>
+      <p class="text-white text-sm lg:text-lg">
+        chatboxについて
+      </p>
+    </div>
   </div>
 </template>
 
@@ -13,7 +20,7 @@ export default {
 <style lang="scss" scoped>
 .c-title {
   &__bg {
-    background-image: url('');
+    background-image: url('~assets/image/company/company_img@2x.png');
   }
 }
 </style>
