0b8e97493adaed8e342833d4430b1cdeef3688dc
manak1 <mana23happy@hotmail.co.jp>
2020-07-21 18:06:43 +0900

    style: remove unnecessory media query

--- pages/404/index.vue
+++ pages/404/index.vue
@@ -1,24 +1,22 @@
 <template>
-  <section class="text-center container mx-auto pr-2 pl-2">
+  <section class="c-404 text-center container mx-auto pr-2 pl-2">
     <div>
       <p
-        class="errorMessage text-primary font-sans mt-24 text-center mb-12 text-4xl lg:text-6xl lg:block"
+        class="errorMessage text-primary font-bold mt-24 text-center mb-10 lg:mb-12 text-3xl lg:text-5xl lg:block"
       >
         404 Not Found
       </p>
     </div>
     <div class="text-center font-medium">
-      <p class="mb-4 text-sm lg:text-lg">
-        お探しのページは見つかりませんでした
-      </p>
-      <p class="mb-24 text-sm lg:text-lg">
+      <p class="c-404__text text-xs leading-9 mb-4 font-bold lg:text-xl">
+        お探しのページは見つかりませんでした<br />
         URLが間違っているか、ページが存在しません
       </p>
     </div>
-    <div class="flex justify-center">
+    <div class="flex justify-center mt-24 lg:mt-32">
       <nuxt-link
         to="/"
-        class="btn hover:opacity-75 inline-block cursor-pointer bg-primary p-5 rounded text-white text-center lg:w-12 lg:w-2/6 lg:text-lg lg:p-8 lg:duration-600"
+        class="btn hover:opacity-75 inline-block text-sm cursor-pointer bg-primary py-5 px-8 rounded-md text-white text-center lg:w-12 lg:w-2/6 lg:text-lg lg:p-8 lg:duration-600"
       >
         トップページへ戻る
       </nuxt-link>
@@ -27,9 +25,10 @@
 </template>
 
 <style lang="scss" scoped>
-@media (980px) {
-  .errorMessage {
-    font-size: 44px;
+.c-404 {
+  font-family: 'Hiragino Kaku Gothic Pro', serif;
+  &__text {
+    color: #333333;
   }
 }
 </style>
